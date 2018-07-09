935c1fa2173e6629d1d30af5729042bf049305e5
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-07-09 20:28:55 +0900

    Rewrite (arrowR_eq_x), (arrowR_exists_x)

--- 2018/beaf/Basics.v
+++ 2018/beaf/Basics.v
@@ -42,42 +42,46 @@ Module Beaf.
   Proposition arrowR_eq_x : forall a n b x x',
       [a ^{n} b == x] -> [a ^{n} b == x'] -> x = x'.
   Proof.
-    intros a n b x x' Hx Hx'.
+    intros a n b x x' Ex Ex'.
     generalize dependent x'.
-    induction Hx; intros x' Hx';
-      inversion Hx'; subst; try reflexivity.
-    - (* ArrowInd *)
-      apply IHHx2. replace y with y0. assumption.
-      symmetry. apply IHHx1. assumption.
+    induction Ex; intros x' Ex';
+      inversion Ex'; subst; try reflexivity.
+    - (* contradiction *)
+      exfalso. eapply arrowR_0. apply H2.
+    - exfalso. eapply arrowR_0. apply Ex2.
+    - apply IHEx2. replace y with y0. assumption.
+      symmetry. apply IHEx1. assumption.
   Qed.
 
   Proposition arrowR_exists_x : forall a n b,
-      exists x, [a ^{n} b == x].
+      exists x, [a ^{S n} b == x].
   Proof.
     intros a n.
     induction n as [| n' IHn']; intros b.
-    - exists (a ^ b). apply ArrowBaseN. reflexivity.
+    - exists (a ^ b). apply ArrowBaseN.
     - induction b as [| b' IHb'].
-      + exists a. apply ArrowBaseRhs. reflexivity.
-      + destruct IHb' as [y IHb'].
+      + (* b = 0 *)
+        exists 1. constructor.
+      + (* b > 0 *)
+        destruct IHb' as [y IHb'].
         specialize (IHn' y). destruct IHn' as [x IHn'].
-        exists x. apply ArrowInd with y; assumption.
+        exists x. apply ArrowInd with y.
+        assumption. assumption.
   Qed.
 
   Example tetration_arrowR2_equiv : forall a b,
-      [a ^{_2_} b == a ^^ b].
+      [a ^{2} b == a ^^ b].
   Proof.
     intros a b. induction b as [|b' IHb'].
-    - (* b = 1 *)
-      simpl. apply ArrowBaseRhs. reflexivity.
-    - (* b > 1 *)
+    - (* b = 0 *)
+      simpl. apply ArrowBaseRhs.
+    - (* b > 0 *)
       inversion IHb'; subst.
       + simpl. eapply ArrowInd.
-        apply ArrowBaseRhs. reflexivity.
-        apply ArrowBaseN. reflexivity.
+        apply ArrowBaseRhs. apply ArrowBaseN.
       + eapply ArrowInd.
         apply IHb'.
-        apply ArrowBaseN. reflexivity.
+        apply ArrowBaseN.
   Qed.
 
 End Beaf.
